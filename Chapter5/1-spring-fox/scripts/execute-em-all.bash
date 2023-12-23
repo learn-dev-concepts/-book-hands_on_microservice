11914e9c637086bc101042179ace49cbd1cdd5fe
devsince2021 <dev.ryu2021@gmail.com>
2023-12-23 17:09:06 +0900

    Ch5: change execute script

--- Chapter5/1-spring-fox/scripts/execute-em-all.bash
+++ Chapter5/1-spring-fox/scripts/execute-em-all.bash
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 
-
-java -jar microservices/product-composite-service/build/libs/product-composite-service-1.0.0-SNAPSHOT.jar &
-java -jar microservices/product-service/build/libs/product-service-1.0.0-SNAPSHOT.jar &
-java -jar microservices/recommendation-service/build/libs/recommendation-service-1.0.0-SNAPSHOT.jar &
-java -jar microservices/review-service/build/libs/review-service-1.0.0-SNAPSHOT.jar &
+docker-compose up -d
