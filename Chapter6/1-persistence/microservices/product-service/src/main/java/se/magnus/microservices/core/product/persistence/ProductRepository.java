660caa77461aaf7bb9b702d67be92fd3cd362f7e
devsince2021 <dev.ryu2021@gmail.com>
2023-12-24 16:45:34 +0900

    Ch6: fix mongodb indexed annotation issue

--- Chapter6/1-persistence/microservices/product-service/src/main/java/se/magnus/microservices/core/product/persistence/ProductRepository.java
+++ Chapter6/1-persistence/microservices/product-service/src/main/java/se/magnus/microservices/core/product/persistence/ProductRepository.java
@@ -1,9 +1,8 @@
 package se.magnus.microservices.core.product.persistence;
 
-import org.springframework.data.repository.PagingAndSortingRepository;
-
+import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.Optional;
 
-public interface ProductRepository extends PagingAndSortingRepository<ProductEntity, String> {
+public interface ProductRepository extends MongoRepository<ProductEntity, String> {
   Optional<ProductEntity> findByProductId(int productId);
 }
