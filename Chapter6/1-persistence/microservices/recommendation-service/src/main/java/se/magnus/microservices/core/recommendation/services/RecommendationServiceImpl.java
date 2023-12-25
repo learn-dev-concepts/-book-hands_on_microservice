ef4e84729b4c6c4aeaf3f1f22d7993c823969ebe
devsince2021 <dev.ryu2021@gmail.com>
2023-12-25 15:31:41 +0900

    Ch6: update recommendation domain

--- Chapter6/1-persistence/microservices/recommendation-service/src/main/java/se/magnus/microservices/core/recommendation/services/RecommendationServiceImpl.java
+++ Chapter6/1-persistence/microservices/recommendation-service/src/main/java/se/magnus/microservices/core/recommendation/services/RecommendationServiceImpl.java
@@ -1,11 +1,14 @@
 package se.magnus.microservices.core.recommendation.services;
 
+import org.springframework.dao.DuplicateKeyException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 import se.magnus.api.core.recommendation.Recommendation;
 import se.magnus.api.core.recommendation.RecommendationService;
+import se.magnus.microservices.core.recommendation.persistence.RecommendationEntity;
+import se.magnus.microservices.core.recommendation.persistence.RecommendationRepository;
 import se.magnus.util.exceptions.InvalidInputException;
 import se.magnus.util.http.ServiceUtil;
 
@@ -15,10 +18,16 @@ import java.util.List;
 @RestController
 public class RecommendationServiceImpl implements RecommendationService {
   private static final Logger LOG = LoggerFactory.getLogger(RecommendationServiceImpl.class);
+
+  private final RecommendationRepository repository;
+
+  private final RecommendationMapper mapper;
   private final ServiceUtil serviceUtil;
 
   @Autowired
-  public RecommendationServiceImpl(ServiceUtil serviceUtil) {
+  public RecommendationServiceImpl(RecommendationRepository repository, RecommendationMapper mapper, ServiceUtil serviceUtil) {
+    this.repository = repository;
+    this.mapper = mapper;
     this.serviceUtil = serviceUtil;
   }
 
@@ -39,5 +48,26 @@ public class RecommendationServiceImpl implements RecommendationService {
     LOG.debug("/recommendation response size: {}", list.size());
 
     return list;
+  }
+
+  @Override
+  public Recommendation createRecommendation(Recommendation body) {
+    try {
+      RecommendationEntity entity = mapper.apiToEntity(body);
+      RecommendationEntity newEntity = repository.save(entity);
+
+      LOG.debug("createRecommendation: created a recommendation entity: {}/{}", body.getProductId(), body.getRecommendationId());
+      return mapper.entityToApi(newEntity);
+
+    } catch (DuplicateKeyException dke) {
+      throw new InvalidInputException("Duplicate key, Product Id: " + body.getProductId() + ", Recommendation Id:" + body.getRecommendationId());
+    }
+  }
+
+
+  @Override
+  public void deleteRecommendations(int productId) {
+    LOG.debug("deleteRecommendations: tries to delete recommendations for the product with productId: {}", productId);
+    repository.deleteAll(repository.findByProductId(productId));
   }
 }
