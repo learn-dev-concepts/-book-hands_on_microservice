b514eb18e10169c662d0c3b3131da9765ce07df7
devsince2021 <dev.ryu2021@gmail.com>
2023-12-29 20:35:01 +0900

    Ch6: build success

--- Chapter06-1/microservices/recommendation-service/src/test/java/se/magnus/microservices/core/recommendation/RecommendationServiceApplicationTests.java
+++ Chapter06-1/microservices/recommendation-service/src/test/java/se/magnus/microservices/core/recommendation/RecommendationServiceApplicationTests.java
@@ -18,7 +18,7 @@ import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static reactor.core.publisher.Mono.just;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment=RANDOM_PORT, properties = {"spring.data.mongodb.port: 0"})
+@SpringBootTest(webEnvironment=RANDOM_PORT)
 public class RecommendationServiceApplicationTests {
 
 	@Autowired
