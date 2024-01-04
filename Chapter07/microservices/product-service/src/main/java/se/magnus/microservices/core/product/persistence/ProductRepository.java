90958cecc524bf8c25186457db89d9c972501356
devsince2021 <dev.ryu2021@gmail.com>
2024-01-04 14:42:11 +0900

    ch7'

--- Chapter07/microservices/product-service/src/main/java/se/magnus/microservices/core/product/persistence/ProductRepository.java
+++ Chapter07/microservices/product-service/src/main/java/se/magnus/microservices/core/product/persistence/ProductRepository.java
@@ -1,10 +1,12 @@
 package se.magnus.microservices.core.product.persistence;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import reactor.core.publisher.Mono;
 
 import java.util.Optional;
 
-public interface ProductRepository extends MongoRepository<ProductEntity, String> {
-    Optional<ProductEntity> findByProductId(int productId);
+public interface ProductRepository extends ReactiveMongoRepository<ProductEntity, String> {
+    Mono<ProductEntity> findByProductId(int productId);
 }
--- Chapter07/microservices/recommendation-service/src/main/java/se/magnus/microservices/core/recommendation/persistence/RecommendationRepository.java
+++ Chapter07/microservices/recommendation-service/src/main/java/se/magnus/microservices/core/recommendation/persistence/RecommendationRepository.java
@@ -1,9 +1,11 @@
 package se.magnus.microservices.core.recommendation.persistence;
 
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.data.repository.CrudRepository;
+import reactor.core.publisher.Flux;
 
 import java.util.List;
 
-public interface RecommendationRepository extends CrudRepository<RecommendationEntity, String> {
-    List<RecommendationEntity> findByProductId(int productId);
+public interface RecommendationRepository extends ReactiveMongoRepository<RecommendationEntity, String> {
+    Flux<RecommendationEntity> findByProductId(int productId);
 }
